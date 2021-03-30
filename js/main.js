0842b855c8f2232bffd07ae0434ba16d86c69710
Jake <jdfenlason@gmail.com>
2021-03-30 09:39:33 -0400

    Verify that each new entry receives an incrementing entryId.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-/* eslint-disable no-console */
+
 /* global data */
 /* exported data */
 var $imageUrl = document.querySelector('.image-url');
-var $userUrl = document.querySelector('#user-photoUrl');
-
-$userUrl.addEventListener('input', function (event) {
-  var $userInputValue = event.target.value;
-  $imageUrl.setAttribute('src', $userUrl);
-  console.log($userInputValue);
-});
-
+var $userPhotoUrl = document.querySelector('#user-photoUrl');
 var $newEntry = document.querySelector('.journal-entry-form');
-console.log($newEntry);
-console.dir($newEntry);
+
+function entryImageUpdate(event) {
+  var $photoInputValue = event.target.value;
+  $imageUrl.setAttribute('src', $photoInputValue);
+}
+
+$userPhotoUrl.addEventListener('input', entryImageUpdate);
+
 function saveEntry(event) {
   event.preventDefault();
   var userInput = {};
   userInput.title = $newEntry.elements.title.value;
   userInput.photo = $newEntry.elements.photo.value;
-  userInput.note = $newEntry.elements.note.value;
+  userInput.notes = $newEntry.elements.notes.value;
   userInput.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(userInput);
-  console.log(data.nextEntryId);
+  $newEntry.reset();
+  $imageUrl.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
-console.log(saveEntry);
+$newEntry.addEventListener('submit', saveEntry);
