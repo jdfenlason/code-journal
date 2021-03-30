ec0d6275d399658440fc6464b17d0b2e4b377c5f
Jake <jdfenlason@gmail.com>
2021-03-30 15:02:20 -0400

    Add a sibling div[data-view=entries] to the HTML.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 /* global data */
 /* exported data */
+var $imageUrl = document.querySelector('.image-url');
+var $userPhotoUrl = document.querySelector('#user-photoUrl');
+var $newEntry = document.querySelector('.journal-entry-form');
+
+function entryImageUpdate(event) {
+  var $photoInputValue = event.target.value;
+  $imageUrl.setAttribute('src', $photoInputValue);
+}
+
+$userPhotoUrl.addEventListener('input', entryImageUpdate);
+
+function saveEntry(event) {
+  event.preventDefault();
+  var userInput = {};
+  userInput.title = $newEntry.elements.title.value;
+  userInput.photo = $newEntry.elements.photo.value;
+  userInput.notes = $newEntry.elements.notes.value;
+  userInput.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(userInput);
+
+  $newEntry.reset();
+  $imageUrl.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
+$newEntry.addEventListener('submit', saveEntry);
