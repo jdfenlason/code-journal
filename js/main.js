9e185101581685f1f999e225386627a300f6761e
Jake <jdfenlason@gmail.com>
2021-03-29 20:45:41 -0400

    listen for submit events

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 /* global data */
 /* exported data */
+// var $imageUrl = document.querySelector('.image-url');
+// var $userUrl = document.querySelector('#user-photoUrl');
+
+// $userUrl.addEventListener('input', function (event) {
+//   var $userInputValue = event.target.value;
+//   $imageUrl.setAttribute('src', $userUrl);
+// });
+
+// var $newEntry = document.querySelector('.journal-entry-form');
+
+// function saveEntry (event) {
+//   event.preventDefault();
+//   var userInput = {};
+//   userInput.title = $newEntry.elements.title.value;
+//   userInput.photo = $newEntry.elements.photo.value;
+//   userInput.note = $newEntry.elements.note.value;
+//   userInput.entryId = data.nextEntryId;
+//   data.nextEntryId++;
+
+// }
